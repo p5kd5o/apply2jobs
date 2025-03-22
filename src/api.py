98df96a4ab533fc20a5b5d94eaa0d964be5fc433
Patrick DeYoreo <pdeyoreo@gmail.com>
2025-03-22 00:52:52 -0700

    Renamed app.py to api.py

--- src/api.py
+++ src/api.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+api = FastAPI(
+    title="apply2jobs"
+)
 
 origins_regex = [
     r"https?://localhost(:\d+)?",
 ]
-app.add_middleware(
+api.add_middleware(
     CORSMiddleware,
     allow_origin_regex=origins_regex,
     allow_credentials=True,
@@ -14,6 +16,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
+@api.get("/")
 async def root():
     return {"message": "Hello World"}
